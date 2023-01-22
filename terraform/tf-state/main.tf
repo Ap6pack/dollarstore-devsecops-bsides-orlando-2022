63e36e27afe9955a4f2c8769460babc649e74a68
Adam Rhys Heaton <adamslinuxemail@gmail.com>
2023-01-22 13:18:07 -0500

    modifyed: main.tf

--- terraform/tf-state/main.tf
+++ terraform/tf-state/main.tf
@@ -1,53 +1,53 @@
-variable "aws_region" { 
-    default = "us-east-1" 
- } 
-variable "aws_secret_key" {} 
-variable "aws_access_key" {} 
+variable "aws_region" {
+  default = "us-east-1"
+}
+variable "aws_secret_key" {}
+variable "aws_access_key" {}
 variable "aws_token" {}
 
-provider "aws" { 
-    region = var.aws_region 
-    access_key = var.aws_access_key 
-    secret_key = var.aws_secret_key 
-    token = var.aws_token
-} 
+provider "aws" {
+  region     = var.aws_region
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+  token      = var.aws_token
+}
 
-resource "aws_s3_bucket" "terraform_state" { 
-    bucket = "bsides-orlando-terraform-state"
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "bsides-orlando-terraform-state"
 
-    #checkov:skip=CKV_AWS_144:Not including cross-region replication as an example
-    #checkov:skip=CKV_AWS_145:Using a different encryption SSE algo as an example
-    lifecycle { 
-        prevent_destroy = true 
-    } 
+  #checkov:skip=CKV_AWS_144:Not including cross-region replication as an example
+  #checkov:skip=CKV_AWS_145:Using a different encryption SSE algo as an example
+  lifecycle {
+    prevent_destroy = true
+  }
 }
 
 
-resource "aws_s3_bucket_versioning" "state_versioning" { 
-    bucket = aws_s3_bucket.terraform_state.id
+resource "aws_s3_bucket_versioning" "state_versioning" {
+  bucket = aws_s3_bucket.terraform_state.id
 
-    versioning_configuration {
-        status = "Enabled"
-    }   
-} 
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
 
 resource "aws_s3_bucket_public_access_block" "protect_state_bucket" {
-     bucket = aws_s3_bucket.terraform_state.id
+  bucket = aws_s3_bucket.terraform_state.id
 
-     block_public_acls       = true
-     block_public_policy     = true
-     ignore_public_acls      = true
-     restrict_public_buckets = true
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
 
-resource "aws_s3_bucket_server_side_encryption_configuration" "state_encryption"{
-    bucket = aws_s3_bucket.terraform_state.bucket
-    rule { 
-        apply_server_side_encryption_by_default { 
-            sse_algorithm = "AES256"
-        } 
-    } 
+resource "aws_s3_bucket_server_side_encryption_configuration" "state_encryption" {
+  bucket = aws_s3_bucket.terraform_state.bucket
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
 }
 
 
@@ -58,15 +58,15 @@ when disabled
 **/
 
 resource "aws_s3_bucket" "log_bucket" {
-    bucket = "log-bucket"
-    lifecycle { 
-        prevent_destroy = true 
-    } 
+  bucket = "log-bucket"
+  lifecycle {
+    prevent_destroy = true
+  }
 }
 
 resource "aws_s3_bucket_acl" "log_bucket_acl" {
-    bucket = aws_s3_bucket.log_bucket.id
-    acl = "log-delivery-write"
+  bucket = aws_s3_bucket.log_bucket.id
+  acl    = "log-delivery-write"
 }
 
 resource "aws_s3_bucket_logging" "log_mapping" {
@@ -78,28 +78,28 @@ resource "aws_s3_bucket_logging" "log_mapping" {
 
 
 resource "aws_s3_bucket_public_access_block" "protect_log_bucket" {
-     bucket = aws_s3_bucket.log_bucket.id
+  bucket = aws_s3_bucket.log_bucket.id
 
-     block_public_acls       = true
-     block_public_policy     = true
-     ignore_public_acls      = true
-     restrict_public_buckets = true
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
-resource "aws_s3_bucket_server_side_encryption_configuration" "log_encryption"{
-    bucket = aws_s3_bucket.log_bucket.bucket
-    rule { 
-        apply_server_side_encryption_by_default { 
-            sse_algorithm = aws:kms
-        } 
-    } 
+resource "aws_s3_bucket_server_side_encryption_configuration" "log_encryption" {
+  bucket = aws_s3_bucket.log_bucket.bucket
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "aws:kms"
+    }
+  }
 }
 
-resource "aws_s3_bucket_versioning" "log_versioning" { 
-    bucket = aws_s3_bucket.log_bucket.id
+resource "aws_s3_bucket_versioning" "log_versioning" {
+  bucket = aws_s3_bucket.log_bucket.id
 
-    versioning_configuration {
-        status = "Enabled"
-    }   
+  versioning_configuration {
+    status = "Enabled"
+  }
 }
 
